dd1a046ea443c35063a7a8b7591a2066212a2faa
bornasinovcic <sinovcic.borna@gmail.com>
2024-07-06 23:31:04 +0200

    refactored cypress code

--- cypress/cypress/e2e/cityCreateUpdateDelete.cy.js
+++ cypress/cypress/e2e/cityCreateUpdateDelete.cy.js
@@ -2,134 +2,75 @@
 
 import { localhost_url } from "../fixtures/example.json";
 
-describe('Testing of city create, update and delete function', () => {
+let _randomNumber = "";
+let _cityName = "";
+let _countryName = "";
 
-    let _randomNumber = "";
-    let _cityName = '';
-    let _countryName = '';
+function searchAndVerify(testCondition = "defaultCondition") {
+    cy.get("input#Name")
+        .clear()
+        .type(_cityName)
+        .should("have.value", _cityName);
+    cy.get("input#Country")
+        .clear()
+        .type(_countryName)
+        .should("have.value", _countryName);
+    cy.get("button.btn.btn-outline-info").click();
+    if (testCondition === "create" || testCondition === "update") {
+        cy.get("tr").contains(_cityName).click();
+        cy.get("dd#name").should("contain", _cityName);
+        cy.get("dd#country").should("contain", _countryName);
+    } else if (testCondition === "delete") {
+        cy.get(".table.table-condensed tbody").find("tr").should("have.length", 0);
+    }
+}
+
+function fillForm() {
+    _randomNumber = Math.floor(10000 + Math.random() * 90000);
+    _cityName = `City${_randomNumber}`;
+    _countryName = `Country${_randomNumber}`;
+    cy.get("input#Name")
+        .clear()
+        .type(_cityName)
+        .should("have.value", _cityName);
+    cy.get("input#Country")
+        .clear()
+        .type(_countryName)
+        .should("have.value", _countryName);
+}
+
+describe("Testing of city create, update and delete function", () => {
 
     beforeEach(() => {
         cy.visit(localhost_url);
     });
 
-    it('Create new city', () => {
-        cy.get('a.nav-link.text-dark[href="/City"]').click();
-        cy.get('a.btn.btn-success[href="/Place/Add"]').click();
-
-        _randomNumber = Math.floor(10000 + Math.random() * 90000);
-        _cityName = `City${randomNumber}`;
-        _countryName = `Country${randomNumber}`;
-
-        cy.get('input#Name')
-            .clear()
-            .type(_cityName)
-            .should('have.value', _cityName);
-        cy.get('input#Country')
-            .clear()
-            .type(_countryName)
-            .should('have.value', _countryName);
-
-        cy.get('input[type="submit"][value="Create"].btn.btn-outline-success.mt-2').click();
-
-        cy.get('input#Name')
-            .clear()
-            .type(_cityName)
-            .should('have.value', _cityName);
-        cy.get('input#Country')
-            .clear()
-            .type(_countryName)
-            .should('have.value', _countryName);
-
-        cy.get('button.btn.btn-outline-info').click();
-
-        cy.get('tr').contains(_cityName).click();
-
-        cy.get('dd#name').should('contain', _cityName);
-        cy.get('dd#country').should('contain', _countryName);
-
+    it("Create new city", () => {
+        cy.get("a.nav-link.text-dark[href='/City']").click();
+        cy.get("a.btn.btn-success[href='/Place/Add']").click();
+        fillForm();
+        cy.get("input[type='submit'][value='Create'].btn.btn-outline-success.mt-2").click();
+        searchAndVerify("create");
     });
 
-    it('Update already existing city', () => {
-        cy.get('a.nav-link.text-dark[href="/City"]').click();
-
-        cy.get('input#Name')
-            .clear()
-            .type(_cityName)
-            .should('have.value', _cityName);
-        cy.get('input#Country')
-            .clear()
-            .type(_countryName)
-            .should('have.value', _countryName);
-
-        cy.get('button.btn.btn-outline-info').click();
-
-        cy.contains('tr', _cityName).within(() => {
-            cy.get('a.btn.btn-primary').click();
+    it("Update already existing city", () => {
+        cy.get("a.nav-link.text-dark[href='/City']").click();
+        searchAndVerify();
+        cy.contains("tr", _cityName).within(() => {
+            cy.get("a.btn.btn-primary").click();
         });
-
-        _randomNumber = Math.floor(10000 + Math.random() * 90000);
-        _cityName = `City${randomNumber}`;
-        _countryName = `Country${randomNumber}`;
-
-        cy.get('input#Name')
-            .clear()
-            .type(_cityName)
-            .should('have.value', _cityName);
-        cy.get('input#Country')
-            .clear()
-            .type(_countryName)
-            .should('have.value', _countryName);
-
-        cy.get('input[type="submit"][value="Save edit"].btn.btn-outline-success.mt-2').click();
-
-        cy.get('input#Name')
-            .clear()
-            .type(_cityName)
-            .should('have.value', _cityName);
-        cy.get('input#Country')
-            .clear()
-            .type(_countryName)
-            .should('have.value', _countryName);
-
-        cy.get('button.btn.btn-outline-info').click();
-
-        cy.get('tr').contains(_cityName).click();
-
-        cy.get('dd#name').should('contain', _cityName);
-        cy.get('dd#country').should('contain', _countryName);
-
+        fillForm();
+        cy.get("input[type='submit'][value='Save edit'].btn.btn-outline-success.mt-2").click();
+        searchAndVerify("update");
     });
 
-    it('Delete already existing city', () => {
-        cy.get('a.nav-link.text-dark[href="/City"]').click();
-
-        cy.get('input#Name')
-            .clear()
-            .type(_cityName)
-            .should('have.value', _cityName);
-        cy.get('input#Country')
-            .clear()
-            .type(_countryName)
-            .should('have.value', _countryName);
-
-        cy.get('button.btn.btn-outline-info').click();
-
-        cy.contains('tr', _cityName).within(() => {
-            cy.get('button.btn.btn-danger').click();
+    it("Delete already existing city", () => {
+        cy.get("a.nav-link.text-dark[href='/City']").click();
+        searchAndVerify()
+        cy.contains("tr", _cityName).within(() => {
+            cy.get("button.btn.btn-danger").click();
         });
-
-        cy.get('input#Name')
-            .clear()
-            .type(_cityName)
-            .should('have.value', _cityName);
-        cy.get('input#Country')
-            .clear()
-            .type(_countryName)
-            .should('have.value', _countryName);
-
-        cy.get('button.btn.btn-outline-info').click();
-
-        cy.get('.table.table-condensed tbody').find('tr').should('have.length', 0)
+        searchAndVerify("delete");
     });
 
 })
--- cypress/cypress/e2e/genreCreateUpdateDelete.cy.js
+++ cypress/cypress/e2e/genreCreateUpdateDelete.cy.js
@@ -2,76 +2,64 @@
 
 import { localhost_url } from "../fixtures/example.json";
 
-describe("Testing of genre create, update and delete function", () => {
+let _randomNumber = "";
+let _genreName = "";
 
-    let _randomNumber = "";
-    let _genreName = "";
-    let testCondition = "";
+function searchAndVerify(testCondition = "defaultCondition") {
+    cy.get("input#Name")
+        .clear()
+        .type(_genreName)
+        .should("have.value", _genreName);
+    cy.get("button.btn.btn-outline-info").click();
+    if (testCondition === "create" || testCondition === "update") {
+        cy.get("tr").contains(_genreName).click();
+        cy.get("dd#name").should("contain", _genreName);
+    } else if (testCondition === "delete") {
+        cy.get(".table.table-condensed tbody").find("tr").should("have.length", 0);
+    }
+}
+
+function fillForm() {
+    _randomNumber = Math.floor(10000 + Math.random() * 90000);
+    _genreName = `Genre${_randomNumber}`;
+    cy.get("input#Name")
+        .clear()
+        .type(_genreName)
+        .should("have.value", _genreName);
+}
+
+describe("Testing of genre create, update and delete function", () => {
 
     beforeEach(() => {
         cy.visit(localhost_url);
     });
 
-    afterEach(() => {
-        cy.get("input#Name")
-            .clear()
-            .type(_genreName)
-            .should("have.value", _genreName);
-        cy.get("button.btn.btn-outline-info").click();
-        if (testCondition === "create" || testCondition === "update") {
-            cy.get("tr").contains(_genreName).click();
-            cy.get("dd#name").should("contain", _genreName);
-        } else if (testCondition === "delete") {
-            cy.get(".table.table-condensed tbody").find("tr").should("have.length", 0)
-        }
-    });
-
     it("Create new genre", () => {
         cy.get("a.nav-link.text-dark[href='/Genre']").click();
         cy.get("a.btn.btn-success[href='/Genre/Create']").click();
-        _randomNumber = Math.floor(10000 + Math.random() * 90000);
-        _genreName = `Genre${_randomNumber}`;
-        cy.get("input#Name")
-            .clear()
-            .type(_genreName)
-            .should("have.value", _genreName);
+        fillForm();
         cy.get("input[type='submit'][value='Create'].btn.btn-outline-success.mt-2").click();
-        testCondition = "create"
+        searchAndVerify("create");
     });
 
     it("Update already existing genre", () => {
         cy.get("a.nav-link.text-dark[href='/Genre']").click();
-        cy.get("input#Name")
-            .clear()
-            .type(_genreName)
-            .should("have.value", _genreName);
-
-        cy.get("button.btn.btn-outline-info").click();
+        searchAndVerify();
         cy.contains("tr", _genreName).within(() => {
             cy.get("a.btn.btn-primary").click();
         });
-        _randomNumber = Math.floor(10000 + Math.random() * 90000);
-        _genreName = `Genre${_randomNumber}`;
-        cy.get("input#Name")
-            .clear()
-            .type(_genreName)
-            .should("have.value", _genreName);
-
+        fillForm();
         cy.get("input[type='submit'][value='Save edit'].btn.btn-outline-success.mt-2").click();
-        testCondition = "update"
+        searchAndVerify("update");
     });
 
     it("Delete already existing genre", () => {
         cy.get("a.nav-link.text-dark[href='/Genre']").click();
-        cy.get("input#Name")
-            .clear()
-            .type(_genreName)
-            .should("have.value", _genreName);
-        cy.get("button.btn.btn-outline-info").click();
+        searchAndVerify();
         cy.contains("tr", _genreName).within(() => {
             cy.get("button.btn.btn-danger").click();
         });
-        testCondition = "delete";
+        searchAndVerify("delete");
     });
 
 });
