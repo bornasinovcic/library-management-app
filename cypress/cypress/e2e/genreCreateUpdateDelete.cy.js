49059e49d09b2c00d1d5693075381ba3b3fad7f7
bornasinovcic <sinovcic.borna@gmail.com>
2024-07-06 22:45:17 +0200

    minor changes

--- cypress/cypress/e2e/genreCreateUpdateDelete.cy.js
+++ cypress/cypress/e2e/genreCreateUpdateDelete.cy.js
@@ -2,7 +2,7 @@
 
 import { localhost_url } from "../fixtures/example.json";
 
-describe('Testing of genre create, update and delete function', () => {
+describe("Testing of genre create, update and delete function", () => {
 
     let _randomNumber = "";
     let _genreName = "";
@@ -13,65 +13,65 @@ describe('Testing of genre create, update and delete function', () => {
     });
 
     afterEach(() => {
-        cy.get('input#Name')
+        cy.get("input#Name")
             .clear()
             .type(_genreName)
-            .should('have.value', _genreName);
-        cy.get('button.btn.btn-outline-info').click();
-        if (testCondition === 'create' || testCondition === 'update') {
-            cy.get('tr').contains(_genreName).click();
-            cy.get('dd#name').should('contain', _genreName);
-        } else if (testCondition === 'delete') {
-            cy.get('.table.table-condensed tbody').find('tr').should('have.length', 0)
+            .should("have.value", _genreName);
+        cy.get("button.btn.btn-outline-info").click();
+        if (testCondition === "create" || testCondition === "update") {
+            cy.get("tr").contains(_genreName).click();
+            cy.get("dd#name").should("contain", _genreName);
+        } else if (testCondition === "delete") {
+            cy.get(".table.table-condensed tbody").find("tr").should("have.length", 0)
         }
     });
 
-    it('Create new genre', () => {
-        cy.get('a.nav-link.text-dark[href="/Genre"]').click();
-        cy.get('a.btn.btn-success[href="/Genre/Create"]').click();
+    it("Create new genre", () => {
+        cy.get("a.nav-link.text-dark[href='/Genre']").click();
+        cy.get("a.btn.btn-success[href='/Genre/Create']").click();
         _randomNumber = Math.floor(10000 + Math.random() * 90000);
         _genreName = `Genre${_randomNumber}`;
-        cy.get('input#Name')
+        cy.get("input#Name")
             .clear()
             .type(_genreName)
-            .should('have.value', _genreName);
-        cy.get('input[type="submit"][value="Create"].btn.btn-outline-success.mt-2').click();
+            .should("have.value", _genreName);
+        cy.get("input[type='submit'][value='Create'].btn.btn-outline-success.mt-2").click();
         testCondition = "create"
     });
 
-    it('Update already existing genre', () => {
-        cy.get('a.nav-link.text-dark[href="/Genre"]').click();
-        cy.get('input#Name')
+    it("Update already existing genre", () => {
+        cy.get("a.nav-link.text-dark[href='/Genre']").click();
+        cy.get("input#Name")
             .clear()
             .type(_genreName)
-            .should('have.value', _genreName);
+            .should("have.value", _genreName);
 
-        cy.get('button.btn.btn-outline-info').click();
-        cy.contains('tr', _genreName).within(() => {
-            cy.get('a.btn.btn-primary').click();
+        cy.get("button.btn.btn-outline-info").click();
+        cy.contains("tr", _genreName).within(() => {
+            cy.get("a.btn.btn-primary").click();
         });
         _randomNumber = Math.floor(10000 + Math.random() * 90000);
         _genreName = `Genre${_randomNumber}`;
-        cy.get('input#Name')
+        cy.get("input#Name")
             .clear()
             .type(_genreName)
-            .should('have.value', _genreName);
+            .should("have.value", _genreName);
 
-        cy.get('input[type="submit"][value="Save edit"].btn.btn-outline-success.mt-2').click();
-        testCondition = 'update'
+        cy.get("input[type='submit'][value='Save edit'].btn.btn-outline-success.mt-2").click();
+        testCondition = "update"
     });
 
-    it('Delete already existing genre', () => {
-        cy.get('a.nav-link.text-dark[href="/Genre"]').click();
-        cy.get('input#Name')
+    it("Delete already existing genre", () => {
+        cy.get("a.nav-link.text-dark[href='/Genre']").click();
+        cy.get("input#Name")
             .clear()
             .type(_genreName)
-            .should('have.value', _genreName);
-        cy.get('button.btn.btn-outline-info').click();
-        cy.contains('tr', _genreName).within(() => {
-            cy.get('button.btn.btn-danger').click();
+            .should("have.value", _genreName);
+        cy.get("button.btn.btn-outline-info").click();
+        cy.contains("tr", _genreName).within(() => {
+            cy.get("button.btn.btn-danger").click();
         });
-        testCondition = 'delete';
+        testCondition = "delete";
     });
-    
+
 });
